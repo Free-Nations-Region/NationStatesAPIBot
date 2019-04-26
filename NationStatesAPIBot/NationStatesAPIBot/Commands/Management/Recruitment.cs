1e7c21e0e4e0a7c74699f915410f4a660e7ae7d3
drehtisch <38186302+drehtisch@users.noreply.github.com>
2019-04-27 01:36:29 +0200

    Hotfix: Probably to long message causing bug with /rn command

--- NationStatesAPIBot/NationStatesAPIBot/Commands/Management/Recruitment.cs
+++ NationStatesAPIBot/NationStatesAPIBot/Commands/Management/Recruitment.cs
@@ -44,50 +44,94 @@ namespace NationStatesAPIBot.Commands.Management
         [Command("rn"), Summary("Returns a list of nations which would receive an recruitment telegram")]
         public async Task DoGetRecruitableNations([Remainder, Summary("Number of nations to be returned")]int number)
         {
-            if (PermissionManager.IsAllowed(PermissionType.ManageRecruitment, Context.User))
+            List<Nation> returnNations = new List<Nation>();
+            try
             {
-                if (!ActionManager.receivingRecruitableNation)
+                if (PermissionManager.IsAllowed(PermissionType.ManageRecruitment, Context.User))
                 {
-                    if (number <= 120)
+                    if (!ActionManager.receivingRecruitableNation)
                     {
-                        ActionManager.receivingRecruitableNation = true;
-                        await ReplyAsync(actionQueued);
-                        List<Nation> returnNations = await NationStatesApiController.GetRecruitableNations(number);
-                        foreach (var nation in returnNations)
+                        if (number <= 120)
                         {
-                            await ActionManager.NationStatesApiController.SetNationStatusToAsync(nation, "reserved_manual");
-                        }
-                        StringBuilder builder = new StringBuilder();
-                        builder.AppendLine("-----");
-                        for (int i = 1; i <= returnNations.Count; i++)
-                        {
-                            var nation = returnNations[i - 1];
-                            if (i % 8 == 0)
+                            ActionManager.receivingRecruitableNation = true;
+                            await ReplyAsync(actionQueued);
+                            returnNations = await NationStatesApiController.GetRecruitableNations(number);
+                            foreach (var nation in returnNations)
                             {
-                                builder.AppendLine($"{nation.Name}");
-                                builder.AppendLine("-----");
+                                await ActionManager.NationStatesApiController.SetNationStatusToAsync(nation, "reserved_manual");
+                            }
+                            StringBuilder builder = new StringBuilder();
+                            builder.AppendLine("-----");
+                            var firstReplyStart = $"<@{Context.User.Id}> Your action just finished.{Environment.NewLine}Changed status of {number} nations from 'pending' to 'reserved_manual'.{Environment.NewLine}Recruitable Nations are (each segment for 1 telegram):{Environment.NewLine}";
+                            int replyCount = (number / 40) + (number % 40 != 0 ? 1 : 0);
+
+                            int currentReply = 1;
+                            for (int i = 1; i <= returnNations.Count; i++)
+                            {
+
+                                var nation = returnNations[i - 1];
+                                if (i % 8 == 0)
+                                {
+                                    builder.AppendLine($"{nation.Name}");
+                                    builder.AppendLine("-----");
+                                }
+                                else
+                                {
+                                    builder.Append($"{nation.Name}, ");
+                                }
+                                if (i % 40 == 0)
+                                {
+                                    if (i / 40 == 1)
+                                    {
+                                        await ReplyAsync($"{firstReplyStart} Reply {currentReply}/{replyCount}{Environment.NewLine}{builder.ToString()}");
+                                        builder.Clear();
+                                    }
+                                    else
+                                    {
+                                        await ReplyAsync($"Reply {currentReply}/{replyCount}{Environment.NewLine}{builder.ToString()}");
+                                    }
+                                    currentReply++;
+                                }
+                            }
+                            if (returnNations.Count < 40)
+                            {
+                                await ReplyAsync($"{firstReplyStart}{builder.ToString()}");
                             }
                             else
                             {
-                                builder.Append($"{nation.Name}, ");
+                                if (number % 40 != 0)
+                                {
+                                    await ReplyAsync($"Reply {currentReply}/{replyCount}{Environment.NewLine}{builder.ToString()}");
+                                }
                             }
                         }
-                        await ReplyAsync($"<@{Context.User.Id}> Your action just finished.{Environment.NewLine}Changed status of {number} nations from 'pending' to 'reserved_manual'.{Environment.NewLine}Recruitable Nations are (each segment for 1 telegram):{Environment.NewLine}{builder.ToString()}");
+                        else
+                        {
+                            await ReplyAsync($"{number} exceeds the maximum of 120 Nations (15 Telegrams a 8 recipients) to be returned.");
+                        }
                     }
                     else
                     {
-                        await ReplyAsync($"{number} exceeds the maximum of 120 Nations (15 Telegrams a 8 recipients) to be returned.");
+                        await ReplyAsync($"There is already a /rn command running. Try again later.");
                     }
-                    ActionManager.receivingRecruitableNation = false;
                 }
                 else
                 {
-                    await ReplyAsync($"There is already a /rn command running. Try again later.");
+                    await ReplyAsync(ActionManager.PERMISSION_DENIED_RESPONSE);
                 }
             }
-            else
+            catch (Exception ex)
             {
-                await ReplyAsync(ActionManager.PERMISSION_DENIED_RESPONSE);
+                NationStatesApiController.Log(Discord.LogSeverity.Critical, $"An critical error occured: {ex}");
+                await ReplyAsync($"Something went wrong :( ");
+                foreach(var nation in returnNations)
+                {
+                    await ActionManager.NationStatesApiController.SetNationStatusToAsync(nation, "pending", "reserved_manual");
+                }
+            }
+            finally
+            {
+                ActionManager.receivingRecruitableNation = false;
             }
         }
 
