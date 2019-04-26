09f5828ab4f6c069b029d0cf315020f2fe6004eb
drehtisch <38186302+drehtisch@users.noreply.github.com>
2019-04-27 01:40:48 +0200

    Increase version number

--- NationStatesAPIBot/NationStatesAPIBot/Program.cs
+++ NationStatesAPIBot/NationStatesAPIBot/Program.cs
@@ -11,7 +11,7 @@ namespace NationStatesAPIBot
 {
     class Program
     {
-        public const string versionString = "v2.6";
+        public const string versionString = "v2.6.1";
         static void Main(string[] args)
         {
             try
