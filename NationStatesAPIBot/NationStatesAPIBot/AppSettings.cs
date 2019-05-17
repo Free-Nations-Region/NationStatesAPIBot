4039da314b68c6d8b2ddf2ce0f2383f59139e9e9
drehtisch <38186302+drehtisch@users.noreply.github.com>
2019-05-17 21:58:54 +0200

    WIP: AppSettings

--- NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
+++ NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
@@ -4,7 +4,17 @@ using System.Text;
 
 namespace NationStatesAPIBot
 {
-    class AppSettings
+    public class AppSettings
     {
+        public string ClientKey { get; set; }
+        public string TelegramId { get; set; }
+        public string TelegramSecretKey { get; set; }
+        public string Contact { get; set; }
+        public string DbConnection { get; set; }
+        public string DiscordBotLoginToken { get; set; }
+        public string DiscordBotAdminUser { get; set; }
+        public string NationStatesRegionName { get; set; }
+        public bool EnableCitizenshipOverTime { get; set; }
+        public int RequiredDaysForCitizenship { get; set; }
     }
 }
