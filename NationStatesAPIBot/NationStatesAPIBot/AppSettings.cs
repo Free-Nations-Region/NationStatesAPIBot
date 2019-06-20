37beef8565a46bc3d0fde1b7afcc2e01f1a6e415
drehtisch <38186302+drehtisch@users.noreply.github.com>
2019-06-20 20:08:18 +0200

    Added UserManager

--- NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
+++ NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
@@ -24,5 +24,17 @@ namespace NationStatesAPIBot
         public string NationStatesRegionName { get; set; }
         public char SeperatorChar { get; set; }
         public bool CriteriaCheckOnNations { get; set; }
+
+        public string Configuration
+        {
+            get
+            {
+#if RELEASE
+                return "production";
+#elif DEBUG
+                return "development";
+#endif
+            }
+        }
     }
 }
--- NationStatesAPIBot/NationStatesAPIBot/Types/LoggingEvents.cs
+++ NationStatesAPIBot/NationStatesAPIBot/Types/LoggingEvents.cs
@@ -4,6 +4,7 @@
     {
         //Everything below 10000 is reserved for random log event ids
         DiscordLogEvent = 10000,
+        UserDbAction = 10050,
         UserMessage = 10100,
         PermissionDenied = 10200,
         GetNationStats = 10300,
