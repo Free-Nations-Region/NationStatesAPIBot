636e78c369b04f90b9eea442c424ba6660b6dcec
drehtisch <38186302+drehtisch@users.noreply.github.com>
2020-04-20 21:47:51 +0200

    Revert "Reduce Nation Search Interval"
    This reverts commit 411b2dc1b91415592f1dc3ed68288c4839b37df1.

--- NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
+++ NationStatesAPIBot/NationStatesAPIBot/AppSettings.cs
@@ -10,7 +10,7 @@ namespace NationStatesAPIBot
         public const long API_REQUEST_INTERVAL = 6000000; //0,6 s
         public const long SEND_NON_RECRUITMENTTELEGRAM_INTERVAL = 300000000; //30 s
         public const long SEND_RECRUITMENTTELEGRAM_INTERVAL = 1800000000; //3 m
-        public const long REQUEST_NEW_NATIONS_INTERVAL = 36000000000; //1 h
+        public const long REQUEST_NEW_NATIONS_INTERVAL = 18000000000; //30 m 
         public const long REQUEST_REGION_NATIONS_INTERVAL = 432000000000; //12 h 
         public const string BOT_ADMIN_TERM = "BotFather"; //Change to something else if you don't like the term
         public static readonly string SLEEPTEXT = $"Psst...I'm sleeping.{Environment.NewLine}{Environment.NewLine}Maintenance going on right now. Please be patient. Thank you :)";
